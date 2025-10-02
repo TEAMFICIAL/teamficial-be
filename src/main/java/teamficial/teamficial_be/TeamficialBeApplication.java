e3f19d8f0b3b7d87886a5c39773bfa87b278b63a
dldusgh318 <pololydotoly@gmail.com>
2025-10-02 23:19:29 +0900

    [chore] 프로젝트 설정 (#1)

---
+++ src/main/java/teamficial/teamficial_be/TeamficialBeApplication.java
@@ -0,0 +1,13 @@
+package teamficial.teamficial_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamficialBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamficialBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teamficial/teamficial_be/TeamficialBeApplicationTests.java
@@ -0,0 +1,13 @@
+package teamficial.teamficial_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamficialBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
