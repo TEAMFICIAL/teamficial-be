110f9b73da8a021a89899e26ee4c4c156367f8f2
dldusgh318 <pololydotoly@gmail.com>
2025-10-03 04:25:04 +0900

    chore: redis 설정 (#4)

---
+++ src/main/java/teamficial/teamficial_be/global/redis/RedisService.java
@@ -0,0 +1,60 @@
+package teamficial.teamficial_be.global.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+import teamficial.teamficial_be.global.apiPayload.code.status.ErrorStatus;
+import teamficial.teamficial_be.global.apiPayload.exception.GeneralException;
+
+import java.time.Duration;
+
+@Component
+@RequiredArgsConstructor
+public class RedisService {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @Value("${jwt.token.refresh-expiration-time}")
+    private long refreshTokenExpirationTime;
+
+    @Value("${jwt.token.access-expiration-time}")
+    private long accessExpirationTime;
+
+    public void setRefreshToken(String key, String value) {
+        try {
+            ValueOperations<String, Object> values = redisTemplate.opsForValue();
+            values.set(key, value, Duration.ofMillis(refreshTokenExpirationTime));
+        } catch (Exception e) {
+            throw new GeneralException(ErrorStatus.REDIS_ERROR);
+        }
+    }
+
+    public String getValue(String key) {
+        try {
+            ValueOperations<String, Object> values = redisTemplate.opsForValue();
+            if (values.get(key) == null) {
+                return "";
+            }
+            return values.get(key).toString();
+        } catch (Exception e) {
+            throw new GeneralException(ErrorStatus.REDIS_ERROR);
+        }
+    }
+
+    public void deleteValue(String key) {
+        try {
+            redisTemplate.delete(key);
+        } catch (Exception e) {
+            throw new GeneralException(ErrorStatus.REDIS_ERROR);
+        }
+    }
+
+    public boolean checkExistsValue(String key) {
+        try {
+            return redisTemplate.hasKey(key);
+        } catch (Exception e) {
+            throw new GeneralException(ErrorStatus.REDIS_ERROR);
+        }
+    }
+}
