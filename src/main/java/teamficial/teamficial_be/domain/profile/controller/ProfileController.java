103e9640977b52b492785d8f87c4ecefde4f0a40
dldusgh318 <pololydotoly@gmail.com>
2025-10-26 19:55:06 +0900

    docs: 스웨거에 api 설명 추가 (#27)

--- src/main/java/teamficial/teamficial_be/domain/profile/controller/ProfileController.java
+++ src/main/java/teamficial/teamficial_be/domain/profile/controller/ProfileController.java
@@ -1,5 +1,6 @@
 package teamficial.teamficial_be.domain.profile.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import teamficial.teamficial_be.domain.profile.dto.request.ProfileRequestDto;
@@ -13,6 +14,7 @@ public class ProfileController {
     private final ProfileService profileService;
 
     @PostMapping("/profile")
+    @Operation(summary = "프로필 생성", description = "프로필을 생성하는 API입니다.")
     public ApiResponse<ProfileResponseDto> createProfile(@RequestParam Long userId,
                                                          @RequestBody ProfileRequestDto requestDto,
                                                          @RequestParam String objectKey) {
@@ -21,12 +23,14 @@ public class ProfileController {
     }
 
     @GetMapping("/profile/{profileId}")
+    @Operation(summary = "프로필 조회", description = "프로필을 조회하는 API입니다.")
     public ApiResponse<ProfileResponseDto> getProfile(@PathVariable Long profileId) {
         ProfileResponseDto profileResponseDto = profileService.getProfile(profileId);
         return ApiResponse.onSuccess(profileResponseDto);
     }
 
     @PutMapping("/profile/{profileId}")
+    @Operation(summary = "프로필 수정", description = "프로필을 수정하는 API입니다.")
     public ApiResponse<ProfileResponseDto> updateProfile(@PathVariable Long profileId,
                                                          @RequestBody ProfileRequestDto profileRequestDto){
         ProfileResponseDto profileResponseDto = profileService.updateProfile(profileId, profileRequestDto);
@@ -34,18 +38,21 @@ public class ProfileController {
     }
 
     @PutMapping("profile/{profileId}/image")
+    @Operation(summary = "프로필 사진 수정", description = "프로필 사진을 수정하는 API입니다.")
     public ApiResponse<String> updateProfileImage(@PathVariable Long profileId, @RequestParam String objectKey) {
         String newProfileImage = profileService.updateProfileImage(profileId, objectKey);
         return ApiResponse.onSuccess(newProfileImage);
     }
 
     @DeleteMapping("profile/{profileId}")
+    @Operation(summary = "프로필 삭제", description = "프로필을 삭제하는 API입니다.")
     public ApiResponse<String> deleteProfile(@PathVariable Long profileId) {
         profileService.deleteProfile(profileId);
         return ApiResponse.onSuccess("프로필이 삭제되었습니다.");
     }
 
     @DeleteMapping("/profile/{profileId}/image")
+    @Operation(summary = "프로필 사진 삭제", description = "프로필 사진을 삭제하는 API입니다.")
     public ApiResponse<String> deleteProfileImage(@PathVariable Long profileId) {
         profileService.deleteProfileImage(profileId);
         return ApiResponse.onSuccess("프로필 사진이 삭제되었습니다.");
