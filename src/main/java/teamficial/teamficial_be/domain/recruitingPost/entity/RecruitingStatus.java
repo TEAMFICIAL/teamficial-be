693543eea5578ec54bddea8ab7a7768289cd5453
mino <dlalsgh6772@gmail.com>
2025-10-28 20:23:24 +0900

    feat : 모집 글 상태 enum 수정 (#26)

--- src/main/java/teamficial/teamficial_be/domain/recruitingPost/entity/RecruitingStatus.java
+++ src/main/java/teamficial/teamficial_be/domain/recruitingPost/entity/RecruitingStatus.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum RecruitingStatus {
     OPEN("모집중"),
-    CLOSED("모집완료");
+    CONFIRMED("참여 확정"),
+    CLOSED("모집 마감");
     private final String description;
 }
