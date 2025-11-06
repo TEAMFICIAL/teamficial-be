97c13e8b2c168ea1f5c870a6345d9eee4ac52b1f
mino <dlalsgh6772@gmail.com>
2025-11-06 15:33:34 +0900

    feat : 팀피셜관련 엔티티 구현(#62)

---
+++ src/main/java/teamficial/teamficial_be/domain/keyword/entity/Keyword.java
@@ -0,0 +1,33 @@
+package teamficial.teamficial_be.domain.keyword.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import teamficial.teamficial_be.domain.profile.entity.Profile;
+import teamficial.teamficial_be.domain.user.entity.User;
+import teamficial.teamficial_be.global.entity.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Keyword extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "keyword_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "keyword_name", length = 50)
+    private String keywordName;
+
+    private boolean is_head;
+
+    private Integer count;
+}
